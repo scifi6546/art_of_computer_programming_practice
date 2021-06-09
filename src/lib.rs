9755354e26d4e571773b8846bc7e0d3c8e53ef75
Nick Alexeev <nickalexeev@protonmail.com>
2021-06-09 13:25:01 -0800

    added buddy allocator

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,24 @@
 enum BinaryTree {
-    data(Free),
-    root(Box<[BinaryTree; 2]>),
+    Data(Free),
+    Root(Box<[BinaryTree; 2]>),
 }
 impl BinaryTree {
     pub fn get(&self, index: usize) -> Free {
         match self {
-            Self::data(free) => {
+            Self::Data(free) => {
                 if index == 0 {
                     *free
                 } else {
                     panic!("invalid index")
                 }
             }
-            Self::root(data) => data[index & 1].get(index >> 1),
+            Self::Root(data) => data[index & 1].get(index >> 1),
         }
     }
     /// Gets first element with free root and sets it to used
     pub fn get_first_free(&mut self, traverse_levels: usize) -> Option<usize> {
         match self {
-            Self::data(free) => {
+            Self::Data(free) => {
                 if traverse_levels == 0 {
                     if *free == Free::Free {
                         *free = Free::Used;
@@ -29,13 +29,13 @@ impl BinaryTree {
                 } else {
                     match free {
                         Free::Free => {
-                            *self = BinaryTree::root(Box::new([
-                                BinaryTree::data(Free::Free),
-                                BinaryTree::data(Free::Free),
+                            *self = BinaryTree::Root(Box::new([
+                                BinaryTree::Data(Free::Free),
+                                BinaryTree::Data(Free::Free),
                             ]));
                             match self {
-                                BinaryTree::data(_) => panic!("impossible condition"),
-                                BinaryTree::root(data) => {
+                                BinaryTree::Data(_) => panic!("impossible condition"),
+                                BinaryTree::Root(data) => {
                                     Some(data[0].get_first_free(traverse_levels - 1).unwrap() << 1)
                                 }
                             }
@@ -44,7 +44,7 @@ impl BinaryTree {
                     }
                 }
             }
-            Self::root(data) => {
+            Self::Root(data) => {
                 if traverse_levels > 0 {
                     if let Some(first_try) = data[0].get_first_free(traverse_levels - 1) {
                         Some(first_try << 1)
@@ -74,16 +74,44 @@ pub struct Allocation<'a> {
     alloc_index: usize,
 }
 impl BuddyAllocator {
-    const MIN_BLOCK_SIZE: usize = 1024 * 64;
+    const STARTING_BLOCK_POW: usize = 8;
+    const MIN_BLOCK_SIZE: usize = 1 << Self::STARTING_BLOCK_POW;
     const BLOCK_LEVELS: usize = 4;
     pub fn new() -> Self {
         Self {
-            free_tree: BinaryTree::data(Free::Free),
+            free_tree: BinaryTree::Data(Free::Free),
             data: [0; Self::BLOCK_LEVELS * Self::MIN_BLOCK_SIZE],
         }
     }
-    pub fn alloc(&self, allocation_size: usize) -> Allocation {
-        todo!()
+    pub fn alloc(&mut self, allocation_size: usize) -> Option<Allocation> {
+        let depth_in_tree = Self::BLOCK_LEVELS - Self::get_block_level(allocation_size);
+        if let Some(alloc_index) = self.free_tree.get_first_free(depth_in_tree) {
+            let mem_index = Self::get_alloc_memory_index(alloc_index);
+            let data = &self.data[mem_index..mem_index + allocation_size];
+            Some(Allocation { data, alloc_index })
+        } else {
+            None
+        }
+    }
+    fn get_alloc_memory_index(alloc_index: usize) -> usize {
+        (0..Self::BLOCK_LEVELS)
+            .map(|i| ((alloc_index >> i) & 1) << Self::STARTING_BLOCK_POW)
+            .sum::<usize>()
+    }
+    fn get_block_level(alloc_size: usize) -> usize {
+        let num_blocks = alloc_size / Self::MIN_BLOCK_SIZE
+            + if alloc_size % Self::MIN_BLOCK_SIZE == 1 {
+                1
+            } else {
+                0
+            };
+        let mut max_size = 0;
+        for i in 0..std::mem::size_of::<usize>() {
+            if (num_blocks >> i) & 1 == 1 {
+                max_size = i;
+            };
+        }
+        return max_size;
     }
 }
 #[cfg(test)]
@@ -97,5 +125,17 @@ mod tests {
     fn allocate() {
         let mut tree = BuddyAllocator::new();
         let alloc = tree.alloc(10);
+    }
+    #[test]
+    fn test_block_level() {
+        assert_eq!(BuddyAllocator::get_block_level(4), 0);
+        assert_eq!(
+            BuddyAllocator::get_block_level(BuddyAllocator::MIN_BLOCK_SIZE + 1),
+            1
+        );
+        assert_eq!(
+            BuddyAllocator::get_block_level(BuddyAllocator::MIN_BLOCK_SIZE * 3 + 1),
+            2
+        );
     }
 }
